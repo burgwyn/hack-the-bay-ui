c37e92f4e9a64fe10af5caf28ed95a5e5092aa19
Nat Burgwyn <burgwyn@gmail.com>
2020-08-14 18:28:20 -0400

    Add Hack the Bay branding, remove default styling

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Header from './Header';
 
-test('renders learn react link', () => {
+test('renders hack the bay', () => {
     const { getByText } = render(<Header />);
-    const linkElement = getByText(/learn react/i);
+    const linkElement = getByText(/hack the bay/i);
     expect(linkElement).toBeInTheDocument();
 });
