776bf751ce0dfb5e64f426a2f456eaae7f16f971
Nat Burgwyn <burgwyn@gmail.com>
2020-08-14 18:37:44 -0400

    Add USWDS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'uswds/dist/js/uswds.min';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
